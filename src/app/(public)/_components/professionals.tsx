580274faf6816be7f1293ef16cd7543b1669ce7c
Pierre Paulo <pierresantista@hotmail.com>
2025-11-18 15:48:45 -0300

    feat(home): lista clinicas na pagina inicial

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 import fotoImg from "../../../../public/foto1.png";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { User } from "@prisma/client";
 
-export function Professionals() {
+interface ProfessionalsProps {
+  professionals: User[];
+}
+
+export function Professionals({ professionals }: ProfessionalsProps) {
   return (
     <section className="bg-gray-50 py-16">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -13,153 +18,47 @@ export function Professionals() {
         </h2>
 
         <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <Card className="overflow-hidden">
-            <CardContent className="p-0">
-              <div>
-                <div className="relative h-48">
-                  <Image
-                    src={fotoImg}
-                    alt="Foto da clinica"
-                    fill
-                    className="object-cover"
-                  />
+          {professionals.map((clinic) => (
+            <Card
+              className="overflow-hidden p-0 hover:shadow-lg duration-300"
+              key={clinic.id}
+            >
+              <CardContent className="p-0">
+                <div>
+                  <div className="relative h-48">
+                    <Image
+                      src={clinic.image ?? fotoImg}
+                      alt="Foto da clinica"
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="p-4 space-y-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <h3 className="font-semibold">Clinica centro</h3>
-                    <p className="text-sm text-gray-600">
-                      Rua x, Centro, Manhuacu - MG
-                    </p>
+                <div className="p-4 space-y-4">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <h3 className="font-semibold">{clinic.name} </h3>
+                      <p className="text-sm text-gray-600">
+                        {clinic.address ?? "Endereço não informado"}
+                      </p>
+                    </div>
+
+                    <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
                   </div>
 
-                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
-                </div>
-
-                <Link
-                  href="/clinica/123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center
+                  <Link
+                    href={`/clinica/${clinic.id}`}
+                    className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center
                   justify-center py-2 rounded-md text-sm md:text-base font-medium"
-                >
-                  Agendar Horario
-                  <ArrowRight className="ml-2" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-
-          <Card className="overflow-hidden">
-            <CardContent className="p-0">
-              <div>
-                <div className="relative h-48">
-                  <Image
-                    src={fotoImg}
-                    alt="Foto da clinica"
-                    fill
-                    className="object-cover"
-                  />
+                  >
+                    Agendar Horario
+                    <ArrowRight className="ml-2" />
+                  </Link>
                 </div>
-              </div>
-
-              <div className="p-4 space-y-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <h3 className="font-semibold">Clinica centro</h3>
-                    <p className="text-sm text-gray-600">
-                      Rua x, Centro, Manhuacu - MG
-                    </p>
-                  </div>
-
-                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
-                </div>
-
-                <Link
-                  href="/clinica/123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center
-                  justify-center py-2 rounded-md text-sm md:text-base font-medium"
-                >
-                  Agendar Horario
-                  <ArrowRight className="ml-2" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-
-          <Card className="overflow-hidden">
-            <CardContent className="p-0">
-              <div>
-                <div className="relative h-48">
-                  <Image
-                    src={fotoImg}
-                    alt="Foto da clinica"
-                    fill
-                    className="object-cover"
-                  />
-                </div>
-              </div>
-
-              <div className="p-4 space-y-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <h3 className="font-semibold">Clinica centro</h3>
-                    <p className="text-sm text-gray-600">
-                      Rua x, Centro, Manhuacu - MG
-                    </p>
-                  </div>
-
-                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
-                </div>
-
-                <Link
-                  href="/clinica/123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center
-                  justify-center py-2 rounded-md text-sm md:text-base font-medium"
-                >
-                  Agendar Horario
-                  <ArrowRight className="ml-2" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-
-          <Card className="overflow-hidden">
-            <CardContent className="p-0">
-              <div>
-                <div className="relative h-48">
-                  <Image
-                    src={fotoImg}
-                    alt="Foto da clinica"
-                    fill
-                    className="object-cover"
-                  />
-                </div>
-              </div>
-
-              <div className="p-4 space-y-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <h3 className="font-semibold">Clinica centro</h3>
-                    <p className="text-sm text-gray-600">
-                      Rua x, Centro, Manhuacu - MG
-                    </p>
-                  </div>
-
-                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
-                </div>
-
-                <Link
-                  href="/clinica/123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center
-                  justify-center py-2 rounded-md text-sm md:text-base font-medium"
-                >
-                  Agendar Horario
-                  <ArrowRight className="ml-2" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          ))}
         </section>
       </div>
     </section>
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,14 +2,18 @@ import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
 import { Professionals } from "./_components/professionals";
+import { getProfessionals } from "./_data-access/get-professionals";
 
-export default function home() {
+export default async function home() {
+  const professionals = await getProfessionals();
+  console.log(professionals);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div>
         <Hero />
-        <Professionals />
+        <Professionals professionals={professionals || []} />
         <Footer />
       </div>
     </div>

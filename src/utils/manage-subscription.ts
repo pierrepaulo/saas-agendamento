63b2ad984b607defecb5adf713fc3dd02e480665
Pierre Paulo <pierresantista@hotmail.com>
2025-11-20 11:37:14 -0300

    feat: adiciona webhook do Stripe e salvar/sincronizar assinaturas no banco

---
+++ src/utils/manage-subscription.ts
@@ -0,0 +1,82 @@
+import prisma from "@/lib/prisma";
+import { stripe } from "@/utils/stripe";
+import { Plan } from "@prisma/client";
+
+/**
+ * salvar, atualizar ou deletar informações das assinaturas (subscription) no banco de dados, sincronizando com stripe.
+ */
+export async function manageSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false,
+  deleteAction = false,
+  type?: Plan
+) {
+  const findUser = await prisma.user.findFirst({
+    where: {
+      stripe_customer_id: customerId,
+    },
+  });
+
+  if (!findUser) {
+    return Response.json(
+      { error: "Falha ao realizar assinatura" },
+      { status: 400 }
+    );
+  }
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: findUser.id,
+    status: subscription.status,
+    priceId: subscription.items.data[0].price.id,
+    plan: type ?? "BASIC",
+  };
+
+  if (subscriptionId && deleteAction) {
+    await prisma.subscription.delete({
+      where: {
+        id: subscriptionId,
+      },
+    });
+
+    return;
+  }
+
+  if (createAction) {
+    try {
+      await prisma.subscription.create({
+        data: subscriptionData,
+      });
+    } catch (err) {
+      console.log("erro al salvar assinatura no banco");
+      console.log(err);
+    }
+  } else {
+    try {
+      const findSubscription = await prisma.subscription.findFirst({
+        where: {
+          id: subscriptionId,
+        },
+      });
+
+      if (!findSubscription) return;
+
+      await prisma.subscription.update({
+        where: {
+          id: findSubscription.id,
+        },
+        data: {
+          status: subscription.status,
+          priceId: subscription.items.data[0].price.id,
+          plan: type ?? "BASIC",
+        },
+      });
+    } catch (err) {
+      console.log("falha ao atualizar assinatura no banco");
+      console.log(err);
+    }
+  }
+}

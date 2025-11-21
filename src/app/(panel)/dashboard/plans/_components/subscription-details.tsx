65ffc1f35b63a0e17fcdeaa588685c4088b26640
Pierre Paulo <pierresantista@hotmail.com>
2025-11-21 14:57:52 -0300

    Feat: cria card para exibir assinatura ativa

---
+++ src/app/(panel)/dashboard/plans/_components/subscription-details.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { subscriptionPlans } from "@/utils/plans";
+import { Subscription } from "@prisma/client";
+
+interface SubscriptionDetailProps {
+  subscription: Subscription;
+}
+
+export function SubscriptionDetail({ subscription }: SubscriptionDetailProps) {
+  const subscriptionInfo = subscriptionPlans.find(
+    (plan) => plan.id === subscription.plan
+  );
+
+  async function handleManageSubscription() {
+    console.log("teste");
+  }
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle className="text-2xl">Seu Plano Atual</CardTitle>
+        <CardDescription> Sua assinatura está ativa!</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <div className="flex items-center justify-between">
+          <h3 className="font-semibold text-lg md:text-xl">
+            {subscription.plan === "BASIC" ? "BASIC" : "PROFISSIONAL"}
+          </h3>
+
+          <div className="bg-green-500 text-white w-fit px-4 py-1 rounded-md">
+            {subscription.status === "active" ? "ATIVO" : "INATIVO"}
+          </div>
+        </div>
+
+        <ul className="list-disc list-inside space-y-2 mt-2">
+          {subscriptionInfo &&
+            subscriptionInfo.features.map((feature) => (
+              <li key={feature}>{feature}</li>
+            ))}
+        </ul>
+      </CardContent>
+
+      <CardFooter>
+        <Button onClick={handleManageSubscription}>Gerenciar Assinatura</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/(panel)/dashboard/plans/page.tsx
+++ src/app/(panel)/dashboard/plans/page.tsx
@@ -2,6 +2,7 @@ import getSession from "@/lib/getSession";
 import { redirect } from "next/navigation";
 import { GridPlans } from "./_components/grid-plans";
 import { getSubscription } from "@/utils/get-subscription";
+import { SubscriptionDetail } from "./_components/subscription-details";
 
 export default async function Plans() {
   const session = await getSession();
@@ -17,7 +18,7 @@ export default async function Plans() {
     <div>
       {subscription?.status !== "active" && <GridPlans />}
       {subscription?.status === "active" && (
-        <h1>Você tem uma assinatura ativa</h1>
+        <SubscriptionDetail subscription={subscription!} />
       )}
     </div>
   );

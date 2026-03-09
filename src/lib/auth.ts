8bffe199e06e8a3e6d142e6646ebd5ce1c020aca
Pierre Paulo <pierresantista@hotmail.com>
2026-03-09 17:51:51 -0300

    refactor(public): remover helpers publicos sem uso

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   trustHost: true,
   providers: [GitHub, Google],
